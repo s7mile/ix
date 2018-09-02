9f27d8e6d1f7b5356ce2a3ed0c2fef78bbaf07de
이민정 <haeru@iminjeong-ui-MacBook-Pro.local>
2018-09-03 00:07:57 +0900

    Add 404

---
+++ src/components/views/pagenotfound.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<strong>404</strong>
+		Page Not Found
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'pageNotFound',
+	}
+</script>
+
+<style scoped>
+	div {
+		min-height: calc(100vh - 280px);
+		font-size: 30px;
+		text-align: center;
+	}
+	strong {
+		display: block;
+		font-size: 100px
+	}
+</style>
